e9bdceabfa66aad9628e8427b5a624b048a495eb
Garrett Hoffman <son0hoff@gmail.com>
2023-07-27 03:41:09 -0700

    starting off by importing the two packages being used, one is a pdf reader and another is a text to speech translator. Following documentation on how to setup and utilize various class properties

--- pdf-to-audio-script/main.py
+++ pdf-to-audio-script/main.py
@@ -1,1 +1,9 @@
 import pyttsx3, pypdf
+
+
+pdfreader = pypdf.PdfReader(open("grokking-algorithms.pdf", "rb"))
+num_of_pages = len(pdfreader.pages)
+speaker = pyttsx3.init()
+
+for page_num in range(num_of_pages):
+    pass
