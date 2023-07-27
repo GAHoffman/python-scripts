396098edc1ced5d0c2717bf0d3bc35845a5ce9ab
Garrett Hoffman <son0hoff@gmail.com>
2023-07-27 03:53:05 -0700

    looping through pages, extracting text per page and replacing any new line indicators with blank space

--- pdf-to-audio-script/main.py
+++ pdf-to-audio-script/main.py
@@ -6,4 +6,6 @@ num_of_pages = len(pdfreader.pages)
 speaker = pyttsx3.init()
 
 for page_num in range(num_of_pages):
-    pass
+    text = pdfreader.pages[page_num].extractText()
+    clean_text = text.strip().replace("\n", " ")
+    print(clean_text)
