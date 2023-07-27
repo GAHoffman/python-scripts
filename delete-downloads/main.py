ea8f0fd34c94dce45e27847a6da0f0ca99ca16cb
Garrett Hoffman <son0hoff@gmail.com>
2023-07-27 04:59:20 -0700

    updated for multiple environment variables to make responsive to file types and folder paths. Can arrange to go through a list of folders if needed as well or expanded further. Simple script to use to delete files through the os module

--- delete-downloads/main.py
+++ delete-downloads/main.py
@@ -3,6 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FILE_PATH = os.getenv("FILE_PATH")
+# Separated into two separate env variables, one is for the specific folder path, and the other is for the file type to be deleted
+folder_path = os.getenv("FOLDER_PATH")
+print(folder_path)
 
-print(FILE_PATH)
+file_type = os.getenv("FILE_TYPE")
+
+# put both together in string format as the file_path
+file_path = f"{folder_path}/{file_type}"
+print(file_path)
+
+
+if os.path.isfile(file_path):
+    os.remove(file_path)
+    print("File has successfully been deleted")
+else:
+    print("This file does NOT exist")
